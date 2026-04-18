061b61db24a126376f8e3fe72d0dc767a12adb70
saverio596 <saverio.benedetto96@gmail.com>
2026-04-18 10:27:20 +0200

    eliminazione file superflui

--- src/services/api.js
+++ src/services/api.js
@@ -22,23 +22,3 @@ export const ottieniDettagliRicetta = async (id) => {
   const response = await apiClient.get(`/${id}/information`); // Solo l'endpoint specifico
   return response.data;
 };
-
-/*export const cercaRicetteVeg = async (query) => {
-  const response = await axios.get(`${BASE_URL}/complexSearch`, {
-    params: {
-      apiKey: API_KEY,
-      query: query,
-      diet: 'vegetarian',
-    }
-  });
-  return response.data.results;
-};*/
-
-/*export const ottieniDettagliRicetta = async (id) => {
-  const response = await axios.get(`${BASE_URL}/${id}/information`, {
-    params: {
-      apiKey: API_KEY
-    }
-  });
-  return response.data;
-};*/
